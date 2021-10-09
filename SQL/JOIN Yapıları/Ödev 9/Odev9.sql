04f8703cbb1a5105ea7ae60f22b9bce8d25fb2b0
dilsadkut <kutdilsad2@gmail.com>
2021-10-09 12:09:15 +0300

    Join structures Homework 9 edited

--- SQL/JOIN Yapıları/Ödev 9/Odev9.sql
+++ SQL/JOIN Yapıları/Ödev 9/Odev9.sql
@@ -19,12 +19,15 @@ Kolay Gelsin.
 
 SELECT city.city,country.country  
 FROM country
-INNER JOIN city ON city.country_id = country.country_id;
+INNER JOIN city 
+ON city.country_id = country.country_id;
 
 SELECT payment.payment_id,customer.first_name,customer.last_name
 FROM payment
-INNER JOIN customer ON customer.customer_id = payment.customer_id;
+INNER JOIN customer 
+ON customer.customer_id = payment.customer_id;
 
 SELECT rental.rental_id,customer.first_name,customer.last_name
 FROM rental
-INNER JOIN customer ON customer.customer_id = rental.customer_id;
+INNER JOIN customer 
+ON customer.customer_id = rental.customer_id;
